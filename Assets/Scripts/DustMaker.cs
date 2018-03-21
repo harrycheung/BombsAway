4e8589159aaca749fb3b7b3f63af7853dd9333f0
Harry Cheung <harry.cheung@gmail.com>
2018-03-21 15:41:32 -0700

    Tank parts generate dust

---
+++ Assets/Scripts/DustMaker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustMaker : MonoBehaviour
+{
+
+    public GameObject dustFX;
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+        GameObject dust = Instantiate(dustFX, collision.contacts[0].point, Quaternion.identity);
+        Destroy(dust, dust.GetComponent<ParticleSystem>().main.duration);
+	}
+
+}
